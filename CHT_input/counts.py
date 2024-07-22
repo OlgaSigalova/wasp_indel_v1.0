2cb2b07cbf2d41ccb5d7efc6ab159ccf76a376b2
OlgaSigalova <olga.sigalova88@gmail.com>
2024-07-22 11:51:08 +0200

    Change hetp requirements
    Edit counts.py script so that it doesn't break if heterozygous probabilities are not provided.
    In that case, genotype is calculated as simple sum of alleles (0 - homozygous ref, 1 - heterozygous, 2 - homozygous alt)

--- CHT_input/counts.py
+++ CHT_input/counts.py
@@ -61,11 +61,21 @@ class IndividualVariant(VariantTuple):
         end = start + len(ref)
         hash_value = hash((chrom, position, ref, alt))
         # Extract genotype information
-        if 'NA' in line_data[5:9]:
-            assert(line_data[5:9] == ['NA', 'NA', 'NA', 'NA'])
+        # check if genotype/haplotype
+        if line_data[5] == 'NA':
             haplotype = None
             het_prob = None
             genotype = None
+        # check if heterozygous probabilities are missing
+        elif 'NA' in line_data[6:9]:
+            assert(line_data[6:9] == ['NA', 'NA', 'NA'])
+            haplotype = line_data[5]
+            het_prob = None
+            genotype = None
+            # split haplotype string by "\" or "|", sum the alleles
+            genotype = haplotype.replace('|', '\\')
+            genotype = sum(map(int, genotype.split('\\')))
+            #assert(line_data[5:9] == ['NA', 'NA', 'NA', 'NA'])
         else:
             haplotype = line_data[5]
             het_prob, alt_prob = map(float, line_data[7:9])
